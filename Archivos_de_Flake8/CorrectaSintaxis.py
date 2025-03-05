7db741c423d74dec365fa57454b1a0fac44e547e
Barrantess <abarrantesb25@gmail.com>
2025-03-04 22:45:46 -0600

    Delete Archivos_de_Flake8 directory

--- Archivos_de_Flake8/CorrectaSintaxis.py
+++
@@ -1,9 +0,0 @@
-def CorrectaSintaxis(num1, num2):
-    print("Sintaxis correcta")
-    return num1 + num2
-
-
-x = 10
-y = 20
-
-print(CorrectaSintaxis(x, y))
--- Archivos_de_Flake8/ErrorSintaxis.py
+++
@@ -1,9 +0,0 @@
-def ErrorSintaxis(  num1 ,num2):
-    print("Error de sintaxis")
-    return num1 + num2
-
-
-x = 10
-y = 20
-
-print(ErrorSintaxis(x, y))
