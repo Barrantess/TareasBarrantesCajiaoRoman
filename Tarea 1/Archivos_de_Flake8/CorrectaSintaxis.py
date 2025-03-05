4dfe667248373cea88201f81e99314858097cbc7
Barrantess <abarrantesb25@gmail.com>
2025-03-04 22:45:22 -0600

    Add files via upload

---
+++ Tarea 1/Archivos_de_Flake8/CorrectaSintaxis.py
@@ -0,0 +1,9 @@
+def CorrectaSintaxis(num1, num2):
+    print("Sintaxis correcta")
+    return num1 + num2
+
+
+x = 10
+y = 20
+
+print(CorrectaSintaxis(x, y))
---
+++ Tarea 1/Archivos_de_Flake8/ErrorSintaxis.py
@@ -0,0 +1,9 @@
+def ErrorSintaxis(  num1 ,num2):
+    print("Error de sintaxis")
+    return num1 + num2
+
+
+x = 10
+y = 20
+
+print(ErrorSintaxis(x, y))
